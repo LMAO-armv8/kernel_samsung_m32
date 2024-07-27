8762b70da562730fabe14aa7a919ff6f16eb2027
LMAO-armv8 <chaitanya4g9@sasi.ac.in>
2024-07-28 00:54:06 +0530

    fix build script

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9
-git clone https://github.com/afaneh-toolchain/prebuilts_clang_host_linux-x86_clang-r383902 toolchain/clang/host/linux-x86/clang-r383902/bin/clang
+git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 -b ndk-release-r18 toolchain
+git clone https://github.com/afaneh-toolchain/prebuilts_clang_host_linux-x86_clang-r383902 toolchain/clang/host/linux-x86/clang-r383902/
 
-export CROSS_COMPILE=$(pwd)/toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-androidkernel-
+
+export CROSS_COMPILE=$(pwd)/toolchain/bin/aarch64-linux-androidkernel-
 export CC=$(pwd)/toolchain/clang/host/linux-x86/clang-r383902/bin/clang
 export CLANG_TRIPLE=aarch64-linux-gnu-
 export ARCH=arm64
